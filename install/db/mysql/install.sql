db4b147590427946b0acc88b414921b658a0a2e9
Maxim Vayda <axuior12@gmail.com>
2025-07-12 12:17:24 +0200

    db fix

--- install/db/mysql/install.sql
+++ install/db/mysql/install.sql
@@ -2,20 +2,20 @@ CREATE TABLE IF NOT EXISTS `b_bitup_roles` (
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`name` VARCHAR(255) NOT NULL,
 	`code` VARCHAR(50) NOT NULL,
-	PRIMARY KEY (`id`),
+	PRIMARY KEY (`id`)
 	);
 
 CREATE TABLE IF NOT EXISTS `b_bitup_user_points` (
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`user_id` INT NOT NULL,
 	`points_balance` INT NOT NULL DEFAULT 0,
-	PRIMARY KEY (`id`),
+	PRIMARY KEY (`id`)
 	);
 
 CREATE TABLE IF NOT EXISTS `b_bitup_user_roles` (
 		`user_id` INT NOT NULL,
 		`role_id` INT NOT NULL,
-	PRIMARY KEY (`user_id`, `role_id`),
+	PRIMARY KEY (`user_id`, `role_id`)
 	);
 
 CREATE TABLE IF NOT EXISTS `b_bitup_events` (
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS `b_bitup_events` (
 	`points_for_visit` INT NOT NULL DEFAULT 0,
 	`max_participants` INT NULL DEFAULT 0,
 	`status`  VARCHAR(255) NOT NULL,
-	PRIMARY KEY (`id`),
+	PRIMARY KEY (`id`)
 	);
 
 CREATE TABLE IF NOT EXISTS `b_bitup_event_registrations` (
@@ -37,5 +37,5 @@ CREATE TABLE IF NOT EXISTS `b_bitup_event_registrations` (
 	`event_id` INT NOT NULL,
 	`user_id` INT NOT NULL,
 	`status`  VARCHAR(255) NOT NULL,
-	PRIMARY KEY (`id`),
+	PRIMARY KEY (`id`)
 	);

f3e5bed62d3e9ab6f1f4053d8983b9466c42ca8e
Maxim Vayda <axuior12@gmail.com>
2025-07-12 10:26:20 +0200

    db

--- install/db/mysql/install.sql
+++ install/db/mysql/install.sql
@@ -1,1 +1,40 @@
-kek
+CREATE TABLE IF NOT EXISTS `roles` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(255) NOT NULL,
+	`code` VARCHAR(50) NOT NULL,
+	PRIMARY KEY (`id`),
+	);
+
+CREATE TABLE IF NOT EXISTS `users` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`points_balance` INT NOT NULL DEFAULT 0,
+	PRIMARY KEY (`id`),
+	);
+
+CREATE TABLE IF NOT EXISTS `user_roles` (
+		`user_id` INT NOT NULL,
+		`role_id` INT NOT NULL,
+	PRIMARY KEY (`user_id`, `role_id`),
+	);
+
+CREATE TABLE IF NOT EXISTS `events` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(255) NOT NULL,
+	`description` TEXT NULL,
+	`start_date` DATETIME NOT NULL,
+	`end_date` DATETIME NULL,
+	`location` VARCHAR(255) NULL,
+	`organizer_id` INT NOT NULL,
+	`points_for_visit` INT NOT NULL DEFAULT 0,
+	`max_participants` INT NULL DEFAULT 0,
+	`status`  VARCHAR(255) NOT NULL,
+	PRIMARY KEY (`id`),
+	);
+
+CREATE TABLE IF NOT EXISTS `event_registrations` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`event_id` INT NOT NULL,
+	`user_id` INT NOT NULL,
+	`status`  VARCHAR(255) NOT NULL,
+	PRIMARY KEY (`id`),
+	);
--- install/db/mysql/uninstall.sql
+++ install/db/mysql/uninstall.sql
@@ -1,1 +1,4 @@
-kek
+DROP TABLE IF EXISTS `event_registrations`;
+DROP TABLE IF EXISTS `events`;
+DROP TABLE IF EXISTS `user_roles`;
+DROP TABLE IF EXISTS `roles`;

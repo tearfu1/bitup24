41857ec9ddda8f7439583be76636f4af782c237d
Daniil Lisyachenko <lisyachenko.daniil@gmail.com>
2025-07-12 11:06:01 +0200

    add files

---
+++ install/components/bitrix/bitup24.main/templates/.default/template.php
@@ -0,0 +1,19 @@
+<?php
+use Bitrix\Main\Localization\Loc;
+
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)
+{
+	die();
+}
+?>
+
+<h1>HEY-HEY</h1>
+
+<script>
+	BX.ready(function () {
+		if (top.BX.Account)
+			top.BX.Account.closeSidebarWithReload();
+		else
+			top.location.reload();
+	});
+</script>
